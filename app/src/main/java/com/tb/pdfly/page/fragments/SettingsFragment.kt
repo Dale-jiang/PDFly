7b9f74908f8f5acf873c866ccdb92ec3de19b87c
KK <919578485@qq.com>
2025-10-20 17:04:31 +0800

    设置页

--- app/src/main/java/com/tb/pdfly/page/fragments/SettingsFragment.kt
+++ app/src/main/java/com/tb/pdfly/page/fragments/SettingsFragment.kt
@@ -2,11 +2,33 @@ package com.tb.pdfly.page.fragments
 
 import com.tb.pdfly.databinding.FragmentSettingsBinding
 import com.tb.pdfly.page.base.BaseFragment
+import com.tb.pdfly.page.web.WebViewActivity
+import com.tb.pdfly.parameter.PRIVACY_URL
+import com.tb.pdfly.parameter.WEB_URL_KEY
+import com.tb.pdfly.parameter.toActivity
 
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>(FragmentSettingsBinding::inflate) {
 
     override fun initView() {
 
+        binding?.apply {
+
+            itemPrivacy.setOnClickListener {
+                requireActivity().toActivity<WebViewActivity> {
+                    putExtra(WEB_URL_KEY, PRIVACY_URL)
+                }
+            }
+
+            itemShare.setOnClickListener {
+
+            }
+
+            itemLanguage.setOnClickListener {
+
+            }
+
+
+        }
     }
 
 }
