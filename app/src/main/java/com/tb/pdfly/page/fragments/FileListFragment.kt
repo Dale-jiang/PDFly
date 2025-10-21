748bbff7a3f191f9475a8281ed74af6918deeb4b
KK <919578485@qq.com>
2025-10-21 11:07:40 +0800

    ui更换

---
+++ app/src/main/java/com/tb/pdfly/page/fragments/FileListFragment.kt
@@ -0,0 +1,16 @@
+package com.tb.pdfly.page.fragments
+
+import com.tb.pdfly.databinding.FragmentFileListBinding
+import com.tb.pdfly.page.base.BaseFragment
+
+class FileListFragment : BaseFragment<FragmentFileListBinding>(FragmentFileListBinding::inflate) {
+
+    override fun initView() {
+
+        binding?.apply {
+
+
+        }
+    }
+
+}
