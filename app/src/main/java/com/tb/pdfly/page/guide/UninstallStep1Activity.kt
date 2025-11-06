c4fc623f61a1b0ba47bb4be5b0fcbff5fd02f9e7
KK <919578485@qq.com>
2025-11-06 15:32:28 +0800

    新用户流程广告

--- app/src/main/java/com/tb/pdfly/page/guide/UninstallStep1Activity.kt
+++ app/src/main/java/com/tb/pdfly/page/guide/UninstallStep1Activity.kt
@@ -2,10 +2,19 @@ package com.tb.pdfly.page.guide
 
 import android.content.Intent
 import androidx.activity.addCallback
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import com.tb.pdfly.admob.AdCenter
+import com.tb.pdfly.admob.interfaces.IAd
 import com.tb.pdfly.databinding.ActivityUninstallStep1Binding
 import com.tb.pdfly.page.MainActivity
 import com.tb.pdfly.page.base.BaseActivity
+import com.tb.pdfly.parameter.CallBack
+import com.tb.pdfly.parameter.app
 import com.tb.pdfly.parameter.toActivity
+import com.tb.pdfly.report.ReportCenter
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class UninstallStep1Activity : BaseActivity<ActivityUninstallStep1Binding>(ActivityUninstallStep1Binding::inflate) {
 
@@ -22,9 +31,14 @@ class UninstallStep1Activity : BaseActivity<ActivityUninstallStep1Binding>(Activ
             layoutDiscover.setOnClickListener { goBackHome() }
             btnKeep.setOnClickListener { goBackHome() }
             btnUninstall.setOnClickListener {
-                toActivity<UninstallStep2Activity>(finishCurrent = true)
+                showNextAd{
+                    toActivity<UninstallStep2Activity>(finishCurrent = true)
+                }
             }
         }
+
+        AdCenter.pdflyBackInt.loadAd(app)
+        showNatAd()
 
     }
 
@@ -35,5 +49,42 @@ class UninstallStep1Activity : BaseActivity<ActivityUninstallStep1Binding>(Activ
         }
 
     }
+
+    private fun showNextAd(callBack: CallBack) {
+        ReportCenter.reportManager.report("pdfly_ad_chance", mapOf("ad_pos_id" to "pdfly_uninstall_int"))
+        lifecycleScope.launch {
+            while (!lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) delay(200L)
+            val ad = AdCenter.pdflyScanInt
+            if (ad.canShow(this@UninstallStep1Activity)) {
+                ad.showFullAd(this@UninstallStep1Activity, "pdfly_uninstall_int", showLoading = true) { callBack() }
+            } else {
+                ad.loadAd(this@UninstallStep1Activity)
+                callBack()
+            }
+        }
+    }
+
+    private var ad: IAd? = null
+    private fun showNatAd() {
+        if (AdCenter.adNoNeededShow()) return
+        ReportCenter.reportManager.report("pdfly_ad_chance", hashMapOf("ad_pos_id" to "pdfly_uninstall_nat1"))
+        val nAd = AdCenter.pdflyScanNat
+        nAd.waitingNativeAd(this@UninstallStep1Activity) {
+            if (nAd.canShow(this@UninstallStep1Activity)) {
+                binding.adContainer.apply {
+                    ad?.destroy()
+                    nAd.showNativeAd(this@UninstallStep1Activity, this, "pdfly_uninstall_nat1") {
+                        ad = it
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ad?.destroy()
+    }
+
 
 }
--- app/src/main/java/com/tb/pdfly/page/guide/UninstallStep2Activity.kt
+++ app/src/main/java/com/tb/pdfly/page/guide/UninstallStep2Activity.kt
@@ -5,12 +5,19 @@ import android.provider.Settings
 import androidx.activity.addCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.net.toUri
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import com.tb.pdfly.admob.AdCenter
 import com.tb.pdfly.databinding.ActivityUninstallStep2Binding
 import com.tb.pdfly.page.MainActivity
 import com.tb.pdfly.page.base.BaseActivity
 import com.tb.pdfly.page.dialog.UninstallDialog
+import com.tb.pdfly.parameter.CallBack
 import com.tb.pdfly.parameter.toActivity
+import com.tb.pdfly.report.ReportCenter
 import com.tb.pdfly.utils.applife.HotStartManager
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class UninstallStep2Activity : BaseActivity<ActivityUninstallStep2Binding>(ActivityUninstallStep2Binding::inflate) {
 
@@ -29,20 +36,20 @@ class UninstallStep2Activity : BaseActivity<ActivityUninstallStep2Binding>(Activ
             ivBack.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
             btnKeep.setOnClickListener { goBackHome() }
             btnUninstall.setOnClickListener {
+                showNextAd {
+                    UninstallDialog {
+                        if (it) {
+                            HotStartManager.navigateToSettingPage(true)
+                            detailsResultLauncher.launch(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                data = "package:${packageName}".toUri()
+                                addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                            })
+                        } else {
+                            goBackHome()
+                        }
 
-                UninstallDialog {
-                    if (it) {
-                        HotStartManager.navigateToSettingPage(true)
-                        detailsResultLauncher.launch(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                            data = "package:${packageName}".toUri()
-                            addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-                        })
-                    } else {
-                        goBackHome()
-                    }
-
-                }.show(supportFragmentManager, "uninstall")
-
+                    }.show(supportFragmentManager, "uninstall")
+                }
 
             }
         }
@@ -54,6 +61,23 @@ class UninstallStep2Activity : BaseActivity<ActivityUninstallStep2Binding>(Activ
         toActivity<MainActivity>(finishCurrent = true) {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         }
+    }
 
+    private fun showNextAd(callBack: CallBack) {
+        if (AdCenter.adNoNeededShow()) {
+            callBack()
+            return
+        }
+        ReportCenter.reportManager.report("pdfly_ad_chance", mapOf("ad_pos_id" to "pdfly_uninstall_int2"))
+        lifecycleScope.launch {
+            while (!lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) delay(200L)
+            val ad = AdCenter.pdflyBackInt
+            if (ad.canShow(this@UninstallStep2Activity)) {
+                ad.showFullAd(this@UninstallStep2Activity, "pdfly_uninstall_int2", showLoading = true) { callBack() }
+            } else {
+                ad.loadAd(this@UninstallStep2Activity)
+                callBack()
+            }
+        }
     }
 }
